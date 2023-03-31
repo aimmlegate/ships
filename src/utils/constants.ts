9d33c8c2cb6bee6aab28e2c3c55c5a9b8697d179
Andrey <aimm.legate@gmail.com>
2023-04-01 03:06:40 +0400

    try proxy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,17 +1,8 @@
 export const TIERS = 11;
 
-export const MEDIA_PATH_ = 'https://glossary-wows-global.gcdn.co/icons/';
-export const VEHICLES_PATH_ = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicles/';
-export const NATIONS_PATH_ = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/nations/';
-export const VEHICLES_TYPES_PATH_ =
-  'https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicle_types_common/';
+const proxy = 'https://corsproxy.io/?';
 
-export const MEDIA_PATH = 'https://glossary-wows-global.gcdn.co/icons/';
-export const VEHICLES_PATH =
-  'https://res.cloudinary.com/doneqwi7x/raw/upload/v1680287086/ships/vehicles.json';
-export const NATIONS_PATH =
-  'https://res.cloudinary.com/doneqwi7x/raw/upload/v1680287049/ships/nations.json';
-export const VEHICLES_TYPES_PATH =
-  'https://res.cloudinary.com/doneqwi7x/raw/upload/v1680287035/ships/vehicle_types_common.json';
-
-//TODO: find cors-proxy
+export const MEDIA_PATH = `${proxy}https://glossary-wows-global.gcdn.co/icons/`;
+export const VEHICLES_PATH = `${proxy}https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicles/`;
+export const NATIONS_PATH = `${proxy}https://vortex.worldofwarships.eu/api/encyclopedia/en/nations/`;
+export const VEHICLES_TYPES_PATH = `${proxy}https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicle_types_common/`;
