a2616cd98365e60de09658c63043bfc5d180ce56
Andrey <aimm.legate@gmail.com>
2023-04-01 02:16:16 +0400

    add eslint

--- src/hooks/reactQuery/useNationsQuery.ts
+++ src/hooks/reactQuery/useNationsQuery.ts
@@ -4,11 +4,11 @@ import { API } from '../../api';
 import { Nation } from '../../types';
 
 export function useNationsQuery() {
-    return useQuery<Nation[]>(['nations'], async () => API.fetchNations(), {
-        select: (nation) =>
-            nation
-                .filter((n) => n.tags.includes('inTree'))
-                .filter((n) => !n.tags.includes('hidden'))
-                .sort((a, b) => a.id - b.id),
-    });
+  return useQuery<Nation[]>(['nations'], async () => API.fetchNations(), {
+    select: (nation) =>
+      nation
+        .filter((n) => n.tags.includes('inTree'))
+        .filter((n) => !n.tags.includes('hidden'))
+        .sort((a, b) => a.id - b.id),
+  });
 }
