78e05bbae89e5899ea4174e5831f77d576438fe6
Andrey <aimm.legate@gmail.com>
2023-04-01 23:36:05 +0400

    fix scroll

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,8 +1,40 @@
+import { useEffect } from 'react';
+
 import { useNationsQuery } from '../../hooks/reactQuery/useNationsQuery';
 import { NationSelector } from '../NationSelector';
 
 export const Root = () => {
   const { data } = useNationsQuery();
+
+  useEffect(() => {
+    const root = document.getElementById('root');
+    if (root) {
+      root.onmousedown = () => {
+        let pageX = 0;
+        let pageY = 0;
+
+        document.onmousemove = (e) => {
+          if (pageX !== 0) {
+            root.scrollLeft = root.scrollLeft + (pageX - e.pageX);
+          }
+          if (pageY !== 0) {
+            root.scrollTop = root.scrollTop + (pageY - e.pageY);
+          }
+          pageX = e.pageX;
+          pageY = e.pageY;
+        };
+
+        root.onmouseup = () => {
+          document.onmousemove = null;
+          root.onmouseup = null;
+        };
+
+        root.ondragstart = () => {
+          return false;
+        };
+      };
+    }
+  }, []);
 
   if (!data) {
     return null;
